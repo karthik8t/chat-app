ac91f5577e40892551e5bf90ee1265deb5807003
karthik8t <thatikondakarthik13@gmail.com>
2022-08-21 22:11:37 +0530

    updated socket io

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import React from "react";
 import socketio from "socket.io-client";
-const socketurl = "http://localhost:3001/";
+const socketurl = "https://afternoon-dawn-75866.herokuapp.com/";
 
 export const socket = socketio.connect(socketurl);
 export const SocketContext = React.createContext();
